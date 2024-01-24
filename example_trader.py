c33bafa01ea59b134cbba5278f1b63efc76c0e50
Tamas Kotel <tamas.kotel@thalex.com>
2024-01-24 06:13:09 +0100

    remove keys file

--- example_trader.py
+++ example_trader.py
@@ -4,6 +4,22 @@ import logging
 from typing import Dict, Optional, List
 
 import keys
+# private_keys = {
+#     thalex_py.Network.TEST: """-----BEGIN RSA PRIVATE KEY-----
+# ...
+# -----END RSA PRIVATE KEY-----
+# """,
+#     thalex_py.Network.PROD: """-----BEGIN RSA PRIVATE KEY-----
+# ...
+# -----END RSA PRIVATE KEY-----
+# """
+# }
+#
+# key_ids = {
+#     thalex_py.Network.TEST: "K123456789",
+#     thalex_py.Network.PROD: "K123456789"
+# }
+
 import process_msg
 import thalex_py
 
